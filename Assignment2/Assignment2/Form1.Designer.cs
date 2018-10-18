14184693712cf641ecee7ed85fe2c1d4b9fc3133
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-10-19 09:02:46 +1300

    Wireframe

---
+++ Assignment2/Assignment2/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grp1 = new System.Windows.Forms.GroupBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.txtBxSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lstBx1 = new System.Windows.Forms.ListBox();
+            this.lblDetails = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtbxFirst = new System.Windows.Forms.TextBox();
+            this.txtBxLast = new System.Windows.Forms.TextBox();
+            this.txtBxPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClearLst = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.grp1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grp1
+            // 
+            this.grp1.Controls.Add(this.btnList);
+            this.grp1.Controls.Add(this.btnClearLst);
+            this.grp1.Controls.Add(this.btnDelete);
+            this.grp1.Controls.Add(this.btnUpdate);
+            this.grp1.Controls.Add(this.btnClear);
+            this.grp1.Controls.Add(this.btnAdd);
+            this.grp1.Controls.Add(this.txtBxPhone);
+            this.grp1.Controls.Add(this.txtBxLast);
+            this.grp1.Controls.Add(this.txtbxFirst);
+            this.grp1.Controls.Add(this.lblPhone);
+            this.grp1.Controls.Add(this.lblLastName);
+            this.grp1.Controls.Add(this.lblFirstName);
+            this.grp1.Controls.Add(this.lblDetails);
+            this.grp1.Controls.Add(this.lstBx1);
+            this.grp1.Controls.Add(this.btnSearch);
+            this.grp1.Controls.Add(this.txtBxSearch);
+            this.grp1.Controls.Add(this.lbl1);
+            this.grp1.Location = new System.Drawing.Point(0, 0);
+            this.grp1.Name = "grp1";
+            this.grp1.Size = new System.Drawing.Size(700, 451);
+            this.grp1.TabIndex = 0;
+            this.grp1.TabStop = false;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(22, 16);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(162, 16);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "Enter Customer Name:";
+            // 
+            // txtBxSearch
+            // 
+            this.txtBxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxSearch.Location = new System.Drawing.Point(25, 38);
+            this.txtBxSearch.MaxLength = 50;
+            this.txtBxSearch.Name = "txtBxSearch";
+            this.txtBxSearch.ShortcutsEnabled = false;
+            this.txtBxSearch.Size = new System.Drawing.Size(193, 21);
+            this.txtBxSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(224, 34);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(150, 25);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lstBx1
+            // 
+            this.lstBx1.FormattingEnabled = true;
+            this.lstBx1.Location = new System.Drawing.Point(25, 91);
+            this.lstBx1.Name = "lstBx1";
+            this.lstBx1.Size = new System.Drawing.Size(313, 264);
+            this.lstBx1.TabIndex = 3;
+            // 
+            // lblDetails
+            // 
+            this.lblDetails.AutoSize = true;
+            this.lblDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDetails.Location = new System.Drawing.Point(488, 80);
+            this.lblDetails.Name = "lblDetails";
+            this.lblDetails.Size = new System.Drawing.Size(130, 16);
+            this.lblDetails.TabIndex = 4;
+            this.lblDetails.Text = "Customer Details:";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(422, 125);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(77, 15);
+            this.lblFirstName.TabIndex = 5;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(422, 170);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(76, 15);
+            this.lblLastName.TabIndex = 6;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.Location = new System.Drawing.Point(450, 220);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(48, 15);
+            this.lblPhone.TabIndex = 7;
+            this.lblPhone.Text = "Phone";
+            // 
+            // txtbxFirst
+            // 
+            this.txtbxFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbxFirst.Location = new System.Drawing.Point(504, 124);
+            this.txtbxFirst.MaxLength = 30;
+            this.txtbxFirst.Name = "txtbxFirst";
+            this.txtbxFirst.Size = new System.Drawing.Size(178, 21);
+            this.txtbxFirst.TabIndex = 8;
+            // 
+            // txtBxLast
+            // 
+            this.txtBxLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxLast.Location = new System.Drawing.Point(504, 170);
+            this.txtBxLast.MaxLength = 30;
+            this.txtBxLast.Name = "txtBxLast";
+            this.txtBxLast.Size = new System.Drawing.Size(178, 21);
+            this.txtBxLast.TabIndex = 9;
+            // 
+            // txtBxPhone
+            // 
+            this.txtBxPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBxPhone.Location = new System.Drawing.Point(504, 219);
+            this.txtBxPhone.MaxLength = 12;
+            this.txtBxPhone.Name = "txtBxPhone";
+            this.txtBxPhone.Size = new System.Drawing.Size(178, 21);
+            this.txtBxPhone.TabIndex = 10;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(532, 341);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(150, 30);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(532, 387);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(150, 30);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(359, 341);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(150, 30);
+            this.btnUpdate.TabIndex = 13;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(359, 387);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(150, 30);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnClearLst
+            // 
+            this.btnClearLst.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClearLst.Location = new System.Drawing.Point(194, 386);
+            this.btnClearLst.Name = "btnClearLst";
+            this.btnClearLst.Size = new System.Drawing.Size(150, 30);
+            this.btnClearLst.TabIndex = 15;
+            this.btnClearLst.Text = "CLEAR LIST";
+            this.btnClearLst.UseVisualStyleBackColor = true;
+            // 
+            // btnList
+            // 
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnList.Location = new System.Drawing.Point(25, 386);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(150, 30);
+            this.btnList.TabIndex = 16;
+            this.btnList.Text = "LIST CUSTOMERS";
+            this.btnList.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 450);
+            this.Controls.Add(this.grp1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.grp1.ResumeLayout(false);
+            this.grp1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grp1;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnClearLst;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtBxPhone;
+        private System.Windows.Forms.TextBox txtBxLast;
+        private System.Windows.Forms.TextBox txtbxFirst;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblDetails;
+        private System.Windows.Forms.ListBox lstBx1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.TextBox txtBxSearch;
+    }
+}
+
